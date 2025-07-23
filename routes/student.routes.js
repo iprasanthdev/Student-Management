dbfec0db11c5575a93766f2692ed911e00565147
Prasanth <j.prasanth.me98@gmail.com>
2025-07-23 11:50:37 +0530

    Added basic routes and controller structure

---
+++ routes/student.routes.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const router = express.Router()
+const studentController = require('../controller/student.controller')
+
+
+// @method - GET
+// @action - To get all list of available students or we can limit the count using querry like api\students?limit=5
+router.get('/', studentController.getAllStudents)
+
+// @method - GET
+// @action - To get a paricular students
+router.get('/:id', studentController.getStudent)
+
+// @method - POST
+// @action - To create a students record
+router.post('/', studentController.createStudent)
+
+// @method - PUT
+// @action - To Update a students record
+router.put('/:id', studentController.updateStudent)
+
+// @method - DELETE
+// @action - To Delete a students record from our system
+router.delete('/:id', studentController.deleteStudent)
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
-
+const studentsRoutes = require('./routes/student.routes')
 const PORT = process.env.PORT || 8000
+
+
+app.use('/api/students', studentsRoutes)
 
 app.listen(PORT, () => {
     console.log(`Successfully created server at port ${PORT}`)
